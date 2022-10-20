c5ed589ab5f8a857d1a8fe6848250b563d86603c
Artem Salnikov <a.salnikov00@maill.ru>
2022-10-20 16:00:17 +0300

    fix bugs with alt attribute

--- front/src/components/ticketsList.vue
+++ front/src/components/ticketsList.vue
@@ -1,52 +1,52 @@
 <template>
   <div class="grid__container">
     <figure class="main__block__container">
-      <img class="main__block__image" src="../assets/tickcet.png" />
+      <img class="main__block__image" src="../assets/tickcet.png" alt="Билет"/>
       <figcaption class="figcaption__image">Билет 1</figcaption>
     </figure>
 
     <figure class="main__block__container">
-      <img class="main__block__image" src="../assets/tickcet.png" />
+      <img class="main__block__image" src="../assets/tickcet.png" alt="Билет"/>
       <figcaption class="figcaption__image">Билет 2</figcaption>
     </figure>
 
     <figure class="main__block__container">
-      <img class="main__block__image" src="../assets/tickcet.png" />
+      <img class="main__block__image" src="../assets/tickcet.png" alt="Билет"/>
       <figcaption class="figcaption__image">Билет 3</figcaption>
     </figure>
 
     <figure class="main__block__container">
-      <img class="main__block__image" src="../assets/tickcet.png" />
+      <img class="main__block__image" src="../assets/tickcet.png" alt="Билет"/>
       <figcaption class="figcaption__image">Билет 4</figcaption>
     </figure>
 
     <figure class="main__block__container">
-      <img class="main__block__image" src="../assets/tickcet.png" />
+      <img class="main__block__image" src="../assets/tickcet.png" alt="Билет"/>
       <figcaption class="figcaption__image">Билет 5</figcaption>
     </figure>
 
     <figure class="main__block__container">
-      <img class="main__block__image" src="../assets/tickcet.png" />
+      <img class="main__block__image" src="../assets/tickcet.png" alt="Билет"/>
       <figcaption class="figcaption__image">Билет 6</figcaption>
     </figure>
 
     <figure class="main__block__container">
-      <img class="main__block__image" src="../assets/tickcet.png" />
+      <img class="main__block__image" src="../assets/tickcet.png" alt="Билет"/>
       <figcaption class="figcaption__image">Билет 7</figcaption>
     </figure>
 
     <figure class="main__block__container">
-      <img class="main__block__image" src="../assets/tickcet.png" />
+      <img class="main__block__image" src="../assets/tickcet.png" alt="Билет"/>
       <figcaption class="figcaption__image">Билет 8</figcaption>
     </figure>
 
     <figure class="main__block__container">
-      <img class="main__block__image" src="../assets/tickcet.png" />
+      <img class="main__block__image" src="../assets/tickcet.png" alt="Билет"/>
       <figcaption class="figcaption__image">Билет 9</figcaption>
     </figure>
 
     <figure class="main__block__container">
-      <img class="main__block__image" src="../assets/tickcet.png" />
+      <img class="main__block__image" src="../assets/tickcet.png" alt="Билет"/>
       <figcaption class="figcaption__image">Билет 10</figcaption>
     </figure>
   </div>

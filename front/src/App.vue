c87c163ed634424728f43e5ac342d3a5cd8816b8
Bozhena <bozhena.shalginskaia@mail.ru>
2022-10-20 15:49:26 +0300

    Выполнение задачи

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,8 +1,21 @@
 <template>
-
+<index></index>
+  <nav>
+  <router-link to="/">Home</router-link> |
+  <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
 </template>
 
+<script>
+  export default {
+  name: 'app',
+};
+</script>
+
 <style>
+#app {
 @import '/assets/style.css';
 @import '/assets/style_autorization.css';
+}
 </style>

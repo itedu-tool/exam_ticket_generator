0cabdd9f96effb004e50032200139a32b9119acb
Bozhena <bozhena.shalginskaia@mail.ru>
2022-11-13 18:16:29 +0300

    Выполнение задачи

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,21 +1,43 @@
 <template>
-<index></index>
-  <nav>
-  <router-link to="/">Home</router-link> |
-  <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
-</template>
+  <!-- Шапка.Статичная.Не изменять -->
+  <header>
+    <publicHeader></publicHeader>
+  </header>
 
+  <!-- Основной контент.Вставить свою компоненту -->
+  <main>
+    <publicMain>
+      <index></index>
+      <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </nav>
+      <router-view />
+    </publicMain>
+  </main>
+
+  <!-- Подвал.Статичный.Не изменять -->
+  <footer>
+    <publicFooter></publicFooter>
+  </footer>
+</template>
 <script>
-  export default {
-  name: 'app',
+import publicHeader from './components/publicHeader.vue';
+import publicMain from './components/publicMain.vue';
+import publicFooter from './components/publicFooter.vue';
+
+export default {
+  name: 'App',
+  components: { publicFooter, publicMain, publicHeader },
 };
 </script>
 
 <style>
 #app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 @import '/assets/style.css';
 @import '/assets/style_autorization.css';
 }
 </style>
+
---
+++ front/src/components/publicHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header">
+    <nav class="nav">
+      <div class="nav__body">
+        <span class="nav__text" style="cursor: pointer">Создание билетов</span>
+        <span class="nav__text"><a href="#" style="text-decoration: none; color: inherit">Личный кабинет</a></span>
+      </div>
+    </nav>
+    <div class="title__box">
+      <div class="logo"></div>
+      <div class="title__text">
+        <h1 class="title">Генератор экзаменационных билетов</h1>
+        <p class="text">Программное средство создания<br />экзаменационных билетов <span class="vue">Vue</span>.js</p>
+      </div>
+    </div>
+  </header>
+</template>
+<script></script>
+<style scoped src="@/css/publicHeader.css"></style>

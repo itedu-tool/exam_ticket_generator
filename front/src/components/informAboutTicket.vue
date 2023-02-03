8f8daaf44d99ddaa51f344aabde3b80ff151ef1a
Artem Salnikov <a.salnikov00@maill.ru>
2023-02-03 12:06:10 +0300

    creating an object in InputFields

--- front/src/components/informAboutTicket.vue
+++ front/src/components/informAboutTicket.vue
@@ -1,17 +1,45 @@
 <!-- Контейнер с информацией о билете -->
 <template>
+  <div class="ticket__name">
+    <h1 class="ticket__name__text">Название билета:</h1>
+    <input class="ticket__name__input" type="text" v-model="ticketName" />
+  </div>
+
   <div class="grid__container">
-    <InputFields msg="Цикловая коммисия" />
-    <InputFields msg="Название предмета" />
-    <InputFields msg="Факультет" />
-    <InputFields msg="Экзаменатор" />
-    <InputFields msg="Направление подготовки/Специальность" />
-    <InputFields msg="Председатель ЦК" />
+    <InputFields msg="Цикловая коммисия" @dataTransmission="getStorageData" />
+    <InputFields msg="Название предмета" @dataTransmission="getStorageData" />
+    <InputFields msg="Факультет" @dataTransmission="getStorageData" />
+    <InputFields msg="Экзаменатор" @dataTransmission="getStorageData" />
+    <InputFields msg="Направление подготовки/Специальность" @dataTransmission="getStorageData" />
+    <InputFields msg="Председатель ЦК" @dataTransmission="getStorageData" />
   </div>
 </template>
 
 <script>
 import InputFields from './inputFields.vue';
-export default { components: { InputFields } };
+export default {
+  components: { InputFields },
+  data() {
+    return {
+      ticketName: '',
+      //setArray: [],
+    };
+  },
+  methods: {
+    getStorageData(data) {
+      console.log(data.id);
+    },
+  },
+  mounted() {
+    if (localStorage.ticketName) {
+      this.ticketName = localStorage.ticketName;
+    }
+  },
+  watch: {
+    ticketName(newTicketName) {
+      localStorage.ticketName = newTicketName;
+    },
+  },
+};
 </script>
 <style scoped src="@/css/informAboutTicket.css"></style>
--- front/src/components/inputFields.vue
+++ front/src/components/inputFields.vue
@@ -1,14 +1,28 @@
 <template>
   <!-- Поле ввода информации -->
   <div class="container">
-    <h1 class="input__title">{{ msg }}</h1>
-    <input class="input" type="text" />
+    <label class="input__title" :for="parametrs.id">{{ msg }}</label>
+    <input class="input" type="text" :id="parametrs.id" v-model="parametrs.getInput" @input="dataTransmission" />
   </div>
 </template>
 <script>
 export default {
   props: {
     msg: String,
+  },
+  data() {
+    return {
+      parametrs: {
+        id: `input-${Math.round(Math.random() * 1000)}`,
+        getName: this.msg,
+        getInput: '',
+      },
+    };
+  },
+  methods: {
+    dataTransmission() {
+      this.$emit('dataTransmission', this.parametrs);
+    },
   },
 };
 </script>

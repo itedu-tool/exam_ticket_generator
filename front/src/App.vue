ab786f84ff1593bb43f0071268e08499ef536b54
Артём Сальников <a.salnikov00@mail.ru>
2023-10-03 20:31:06 +0300

    Финальная версия на NodeJS

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <!-- Шапка.Статичная.Не изменять -->
+  <publicHeader></publicHeader>
+
+  <div class="wrapper">
+    <!-- Основной контент.Вставить свою компоненту -->
+    <main class="main">
+      <div class="container">
+        <router-view />
+      </div>
+    </main>
+
+    <!-- Подвал.Статичный.Не изменять -->
+    <publicFooter></publicFooter>
+  </div>
 </template>
+<script>
+import publicHeader from './components/exHeader.vue';
+import publicFooter from './components/exFooter.vue';
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: 'App',
+  components: { publicFooter, publicHeader },
+};
+</script>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,24 +1,109 @@
+//импорт файлов авторизации
+import authorizationTeacher from '../pages/authorization/authorizationTeacher.vue';
+import registrationTeacher from '../pages/registration/registTeacher.vue';
+
+//импорт файлов личного кабинета
+import exPersonalAccount from '../pages/personalAccount/PersonalAccount.vue';
+
+//импорт файлов добавления данных
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+
+import addingData from '../pages/addingData/addingData.vue';
+import viewTicket from '../pages/addingData/ticket-view/viewTicket.vue';
+import ticketExample from '../pages/addingData/ticket-view/ticketExample.vue';
+import firstCreatingTemplate from '../pages/addingData/ticket-creating/firstCreatingTemplate.vue';
+import secondCreatingTemplate from '../pages/addingData/ticket-creating/secondCreatingTemplate.vue';
+import editingTicket from '../pages/addingData/ticket-redact/editingTicketOne.vue';
+import templateEditingTwo from '../pages/addingData/ticket-redact/editingTicketTwo.vue';
+import templateEditingThree from '../pages/addingData/ticket-redact/editingTicketThree.vue';
+import generationTicket from '../pages/addingData/ticket-generation/ticketsGenerationOne.vue';
+import generationTicketTwo from '../pages/addingData/ticket-generation/ticketsGenerationTwo.vue';
 
 const routes = [
+  //роутеры авторизации
   {
-    path: '/',
-    name: 'home',
-    component: HomeView,
+    path: '/authorizationTeacher',
+    name: 'authorizationTeacher',
+    component: authorizationTeacher,
+    alias: '/',
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    path: '/registrationTeacher',
+    name: 'registrationTeacher',
+    component: registrationTeacher,
+  },
+
+  //роутеры личного кабинета
+  {
+    path: '/personalAccount',
+    name: 'personalAccount',
+    component: exPersonalAccount,
+  },
+
+  //роутеры добавления данных
+  {
+    path: '/addingData',
+    name: 'addingData',
+    component: addingData,
+    children: [
+      {
+        path: '/addingData/viewTicket',
+        name: 'viewTicket',
+        component: viewTicket,
+      },
+      {
+        path: '/addingData/viewTicket/ticketExample',
+        name: 'ticketExample',
+        component: ticketExample,
+      },
+      {
+        path: '/addingData/ticket-creating',
+        name: 'ticket-creating',
+        component: firstCreatingTemplate,
+      },
+      {
+        path: '/addingData/ticket-creating/secondCreating',
+        name: 'secondCreating',
+        component: secondCreatingTemplate,
+      },
+      {
+        path: '/addingData/ticket-edit',
+        name: 'ticket-edit',
+        component: editingTicket,
+      },
+      {
+        path: '/addingData/ticket-edit/templateEditingTwo',
+        name: 'templateEditingTwo',
+        component: templateEditingTwo,
+      },
+      {
+        path: '/addingData/ticket-edit/templateEditingThree',
+        name: 'templateEditingThree',
+        component: templateEditingThree,
+      },
+      {
+        path: '/addingData/ticket-generation',
+        name: 'ticket-generation',
+        component: generationTicket,
+      },
+      {
+        path: '/addingData/ticket-generation/successfully',
+        name: 'successfully',
+        component: generationTicketTwo,
+      },
+    ],
+  },
+
+  //обработка неверных маршрутов
+  //task переписать редирект. Создать станицу 404 об ошибке.
+  {
+    path: '/:catchAll(.*)',
+    redirect: { name: 'authorizationTeacher' },
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(process.env.BASE__URL),
   routes,
 });
 
